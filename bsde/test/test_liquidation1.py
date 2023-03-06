035499e425c0fa5afdd75cb8ac1d179b5ddc7351
Yin Fu <fuyin1999@gmail.com>
2023-03-06 04:04:39 -0500

    Update for documentation

--- bsde/test/test_liquidation1.py
+++ bsde/test/test_liquidation1.py
@@ -2,7 +2,6 @@
 # sys.path.insert(0, '/Users/finn/Desktop/Capstone-BSDE/files/bsde')
 
 import numpy as np
-import matplotlib.pyplot as plt
 import tensorflow as tf
 from bsde.config import ConfigHJB1, ConfigLSMC, ConfigDeepBSDE, ConfigFBSNN
 from bsde.solver.lsmc import LSMCLinear
@@ -12,7 +11,7 @@ from bsde.dynamics.liquidation1 import HJB_liquidation1_FBSDE, HJB_liquidation1_
 
 def test_liquidation1_lsmc(cfg_pde, cfg_lsmc_solver):
     """
-    Test the PDE solver using the example in section 6.3
+    Test the PDE solver (LSMC) using the example in section 6.3
     """
     FBSDE = HJB_liquidation1_FBSDE(config=cfg_pde, exclude_spot=True)
     LSMC_solver = LSMCLinear(FBSDE, cfg_lsmc_solver, basis_funcs_type='trig')
@@ -20,16 +19,22 @@ def test_liquidation1_lsmc(cfg_pde, cfg_lsmc_solver):
 
 
 def test_liquidation1_deep(cfg_pde, cfg_deep_solver):
+    """
+    Test the PDE solver (Deep BSDE) using the example in section 6.3
+    """
     FBSDE = HJB_liquidation1_FBSDE(config=cfg_pde, exclude_spot=True)
     deep_solver = DeepBSDESolver(FBSDE, cfg_deep_solver)
     deep_solver.train()
 
 
 def test_liquidation1_FBSNN(cfg_pde, cfg_FBSNN):
+    """
+    Test the PDE solver (FBSNN) using the example in section 6.3
+    """
     HJB_FBSNN = HJB_liquidation1_solver(config_dynamic=cfg_pde, config_solver=cfg_FBSNN)
-    HJB_FBSNN.train(N_Iter=3 * 10 ** 4, learning_rate=7e-3)
-    HJB_FBSNN.train(N_Iter=4 * 10 ** 3, learning_rate=2e-3)
-    HJB_FBSNN.train(N_Iter=3 * 10 ** 3, learning_rate=1e-4)
+    HJB_FBSNN.train(N_Iter=50 * 10 ** 4, learning_rate=1e-3)
+    HJB_FBSNN.train(N_Iter=10 * 10 ** 3, learning_rate=2e-3)
+    HJB_FBSNN.train(N_Iter=10 * 10 ** 3, learning_rate=1e-4)
 
 
 def main():
@@ -43,11 +48,11 @@ def main():
     seed = 42
 
     # Model parameters
-    x0 = np.array([30., 10000.])   # S_0, q_0
-    epsilon = 0.001
+    x0 = np.array([35.0, 10.])   # S_0, q_0 # 7530
+    epsilon = 3e-5
     sig_s = 0.5
-    lb = 1.
-    k = 0.001
+    lb = 1
+    k = 1e-2
 
     print('Analytical Y0: {}'.format(x0[0]*x0[1] - x0[1]**2 / (1/lb + T/k)))
 
@@ -55,13 +60,13 @@ def main():
     cfg_HJB1 = ConfigHJB1(sig_s=sig_s, eps=epsilon, lb=lb, k=k, T=T, d=d, d1=d1, d2=d2)
 
     # FBSNN
-    if True:
+    if False:
         tf.compat.v1.disable_v2_behavior()
-        batch_size = 1
+        batch_size = 8
         cfg_FBSNN = ConfigFBSNN(x0=x0.reshape(1, d), N=N, M=batch_size, dt=dt, seed=seed, layers=[d+1] + 4*[256] + [1])
         test_liquidation1_FBSNN(cfg_pde=cfg_HJB1, cfg_FBSNN=cfg_FBSNN)
 
-    if False:
+    if True:
         # LSMC
         M = 2 ** 16  # N_path
         cfg_lsmc_linear = ConfigLSMC(M=M, N=N, dt=dt, seed=42, x0=x0,
@@ -71,15 +76,15 @@ def main():
     if False:
         # deep BSDE
         tf.compat.v1.enable_v2_behavior()
-        batch_size = 64
+        batch_size = 32
         cfg_deep_solver = ConfigDeepBSDE(N=N, dt=dt, seed=42, x0=x0,
-                                         y_init_range=[-98000, -98100],
-                                         n_hiddens=[10+d, 10+d],
-                                         lr_values=[2e-3, 1e-3],
-                                         lr_boundaries=[6000],
+                                         y_init_range=[x0[0]*x0[1]*0.9, x0[0]*x0[1]],
+                                         n_hiddens=[20+d, 20+d],
+                                         lr_values=[4e-2, 3e-2, 2e-2],
+                                         lr_boundaries=[3000, 5000],
                                          n_iterations=9000,
                                          batch_size=batch_size,
-                                         valid_size=64,
+                                         valid_size=32,
                                          report_freq=100,
                                          dtype='float64',
                                          verbose=True)
